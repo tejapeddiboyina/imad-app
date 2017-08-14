264b4785ba22239697e858078f044b3bb8591597
tejapeddiboyina <30953020+tejapeddiboyina@users.noreply.github.com>
2017-08-14 11:34:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,64 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+    'article-one':{
+        title : 'article One | teja peddiboyina',
+        heading : 'article-One',
+        date : 'aug 14 , 2017',
+        content : '<p>teja\'s article one</p>',
+    },
+     'article-two':{
+        title : 'article two | teja peddiboyina',
+        heading : 'article-two',
+        date : 'aug 14 , 2016',
+        content : '<p>teja\'s article two</p>',
+    },
+     'article-three':{
+        title : 'article three | teja peddiboyina',
+        heading : 'article-three',
+        date : 'aug 14 , 2015',
+        content : '<p>teja\'s article three</p>',
+    }
+};
+function createTemp(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+    var htmlTemp =`
+    <html>
+      <head>
+        <title>${title}</title>
+        <meta name="viewport" content="width=device-width", intial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+      </head>
+      <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <div>
+                <h3>${heading}</h3>
+            </div>
+            <div>
+                ${date}
+            </div>
+             <div>
+                ${content}
+            </div>
+        </div>
+      </body>
+    </html>`;
+    return htmlTemp;
+    }
+
+app.get('/:articlename', function(req,res){
+    var articlename = req.params.articlename;
+    res.send(creatTemp(articles[articlename]));
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
