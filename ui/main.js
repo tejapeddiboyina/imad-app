4b05bf7709ea1ce0d9b16fba5833419397276f0a
tejapeddiboyina <30953020+tejapeddiboyina@users.noreply.github.com>
2017-08-14 18:34:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,26 @@
-console.log('Loaded!');
+// counter code
+var button = document.getElementById('counter');
+
+button.onclick = function(){
+
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpReqyest.DONE){
+            
+        //Take some action
+            if (request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementBId('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        // not done yet.
+    };
+    
+    // Make the reqyest 
+    request.open('GET', 'http://tejapeddiboyina7.imad.hasura-app.io/counter', true);
+    request.send(null);
+}
