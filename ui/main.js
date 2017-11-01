790f5eeb3165cf8da8c32d934a2e2544da8b9134
tejapeddiboyina <30953020+tejapeddiboyina@users.noreply.github.com>
2017-11-02 00:54:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,11 @@
 // counter code
 var button = document.getElementById('counter');
 
+var counter = 0;
+
 button.onclick = function(){
 
-    // Create a request object
+  /*  // Create a request object
     var request = new XMLHttpRequest();
     
     // Capture the response and store it in a variable
@@ -22,5 +24,8 @@ button.onclick = function(){
     
     // Make the reqyest 
     request.open('GET', 'http://tejapeddiboyina7.imad.hasura-app.io/counter', true);
-    request.send(null);
+    request.send(null); */
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
