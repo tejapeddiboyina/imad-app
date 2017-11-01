5ecfdf1db88002f8a4a23115a259225598ebe144
tejapeddiboyina <30953020+tejapeddiboyina@users.noreply.github.com>
2017-11-02 00:56:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 // counter code
-var button = document.getElementById('counter');
+/*var button = document.getElementById('counter');
 
 var counter = 0;
 
@@ -28,4 +28,5 @@ button.onclick = function(){
     counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
-};
+};*/
+console.log("i am teja")
