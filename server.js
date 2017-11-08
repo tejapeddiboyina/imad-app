e472e6ddf90b40725247389c5f7d2238107de6cd
tejapeddiboyina <30953020+tejapeddiboyina@users.noreply.github.com>
2017-11-08 23:03:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ function createTemp(data){
            if(err){
                res.status(500).send(err.toString());
            }else{
-               res.send(JSON.stringify(result));
+               res.send(JSON.stringify(result.rows));
            }
        });
     });
