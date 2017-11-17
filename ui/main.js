cb23d6521da5554431c5ccece0a62c9d5629e958
tejapeddiboyina <30953020+tejapeddiboyina@users.noreply.github.com>
2017-11-17 10:55:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,6 @@
-// counter code
-var button = document.getElementById('counter');
 
-var counter = 0;
-
-button.onclick = function(){
+var submit = document.getElementById('submit-btn');
+submit.onclick = function(){
 
     // Create a request object
     var request = new XMLHttpRequest();
@@ -14,20 +11,22 @@ button.onclick = function(){
             
         //Take some action
             if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+                alert('Logged in successfully');
+            }else if (request.status === 403){
+                alert('Username/password is incorrect');
+            }else if (request.status === 500){
+                alert('Something went wrong on the server');
             }
         }
         // not done yet.
     };
     
     // Make the reqyest 
-    request.open('GET', 'http://tejapeddiboyina7.imad.hasura-app.io/counter', true);
-    request.send(null); 
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+   var username = document.getElementById('username').value;
+   var password - document.getElementById('password').value; 
+   console.log(username);
+   console.log(password);
+   request.open('POST', 'http://tejapeddiboyina7.imad.hasura-app.io/login', true);
+   request.serRequestHeader('Content-Type', 'application/json');
+   request.send(JSON.stringify({username: username, password: password}));
 };
-console.log("i am teja");
-alert('do something');
