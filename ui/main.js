9ececa127293fc30ccefdfb65a440654ab3aa25c
tejapeddiboyina <30953020+tejapeddiboyina@users.noreply.github.com>
2017-11-18 22:18:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -64,29 +64,35 @@ function loadLoginForm(){
       submit.value = 'Logging in....';
     };
     
-    var register = document.getElementById('register-btn');
-    register.onclick = function(){
+    var register = document.getElementById('register_btn');
+    register.onclick = function () {
+        // Create a request object
         var request = new XMLHttpRequest();
-        request.onreadystatechange = function(){
-            if(request.readyState === XMLHttpRequest.DONE){
-                if(request.status === 200){
-                    alert('User created successfully');
-                    register.value = 'Registered';
-                }else {
-                    alert('Could not register the user');
-                    register.value = 'Register';
-                }
-            }
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
         };
         
-        var username = document.getElementById('username');
-        var password = document.getElementById('password');
-        console.log('username');
-        console.log('password');
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
         request.open('POST', '/create-user', true);
         request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));
+        request.send(JSON.stringify({username: username, password: password}));  
         register.value = 'Registering...';
+    
     };
 }
 
